d09e1c14a6bb31b796473fa93365ff53e2116654
Lian Wiebe <lnwiebe05@gmail.com>
2024-06-26 14:41:46 +0200

    profile

--- app/admin/layout.jsx
+++ app/admin/layout.jsx
@@ -1,10 +1,17 @@
+import { assets } from "@/Assets/assets";
 import Sidebar from "@/Components/AdminComponents/Sidebar";
+import Image from "next/image";
 
 export default function Layout({children}){
     return (
         <>
             <div className="flex">
             <Sidebar/>
+            <div className="flex flex-col w-full">
+                <div className="flex items-center justify-between w-full py-3 max-h-[60px] px-12 border-b border-black">
+                    <Image src={assets.profile_icon} width={40} alt="" />
+                </div>
+            </div>
             </div>
             {children}
         </>
