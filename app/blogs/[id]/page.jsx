99c50f4e562dc324bf525e58e31da9418d60f7d4
Lian Wiebe <lnwiebe05@gmail.com>
2024-07-02 13:38:57 +0200

    use html components in the description

--- app/blogs/[id]/page.jsx
+++ app/blogs/[id]/page.jsx
@@ -6,31 +6,31 @@ import Image from 'next/image';
 import Link from 'next/link';
 import React, { useEffect, useState } from 'react'
 
-const page = ({params}) => {
+const page = ({ params }) => {
 
-    const [data,setData] = useState(null);
+  const [data, setData] = useState(null);
 
-    const fetchBlogData = async () =>{
-        const response = await axios.get('/api/blog',{
-          params:{
-            id:params.id
-          }
-        })
-        setData(response.data);
-    }
+  const fetchBlogData = async () => {
+    const response = await axios.get('/api/blog', {
+      params: {
+        id: params.id
+      }
+    })
+    setData(response.data);
+  }
 
-    useEffect(()=>{
-        fetchBlogData();
-    },[])
+  useEffect(() => {
+    fetchBlogData();
+  }, [])
 
-  return (data?<>
+  return (data ? <>
     <div className='bg-gray-200 py-5 px-5 md:px-12 lg:px-28'>
       <div className='flex justify-between items-center'>
         <Link href='/'>
-        <Image src={assets.logo} width={180} alt='' className='w-[130px] sm:w-auto' />
+          <Image src={assets.logo} width={180} alt='' className='w-[130px] sm:w-auto' />
         </Link>
         <button className='flex items-center gap-2 font-medium py-1 px-3 sm:py-3 sm:px-6 border border-black shadow-[-7px_7px_0px_#000000]'>
-            Get started <Image src={assets.arrow} alt='' />
+          Get started <Image src={assets.arrow} alt='' />
         </button>
       </div>
       <div className='text-center my-24'>
@@ -40,31 +40,21 @@ const page = ({params}) => {
       </div>
     </div>
     <div className='mx-5 max-w-[800px] md:mx-auto mt-[-100px] mb-10'>
-        <Image className='border-4 border-white' src={data.image} width={1280} height={720} alt='' />
-        <h1 className='my-8 text-[26px] font-semibold'>Introduction:</h1>
-        <p>{data.description}</p>
-        <h3 className='my-5 text-[18px] font-semibold'>Step 1: Self-Reflection and Goal Setting</h3>
-        <p className='my-3'>Before you can manage your lifestyle, you must have a clear understanding of what you want to achieve. Start by reflecting on your values, aspirations, and long-term goals.</p>
-        <p className='my-3'>Before you can manage your lifestyle, you must have a clear understanding of what you want to achieve. Start by reflecting on your values, aspirations, and long-term goals.</p>
-        <h3 className='my-5 text-[18px] font-semibold'>Step 2: Self-Reflection and Goal Setting</h3>
-        <p className='my-3'>Before you can manage your lifestyle, you must have a clear understanding of what you want to achieve. Start by reflecting on your values, aspirations, and long-term goals.</p>
-        <p className='my-3'>Before you can manage your lifestyle, you must have a clear understanding of what you want to achieve. Start by reflecting on your values, aspirations, and long-term goals.</p>
-        <h3 className='my-5 text-[18px] font-semibold'>Step 3: Self-Reflection and Goal Setting</h3>
-        <p className='my-3'>Before you can manage your lifestyle, you must have a clear understanding of what you want to achieve. Start by reflecting on your values, aspirations, and long-term goals.</p>
-        <p className='my-3'>Before you can manage your lifestyle, you must have a clear understanding of what you want to achieve. Start by reflecting on your values, aspirations, and long-term goals.</p>
-        <h3 className='my-5 text-[18px] font-semibold'>Conclusion</h3>
-        <p className='my-3'>Managing your lifestyle is a journey that requires commitment and sel-awareness. By following this step-by-step guide, you can take control of your life and make meaningful changes that lead to a more balanced and fulfilling lifestyle. Remember that it's okay to seek support and guidance from professionals or mentors along the way. Your well-being and happiness are worth the effort.</p>
-        <div className='my-24'>
-            <p className='text-black font font-semibold my-4'>Share this article on social media</p>
-            <div className='flex'>
-                <Image src={assets.facebook_icon} width={50} alt='' />
-                <Image src={assets.twitter_icon} width={50} alt='' />
-                <Image src={assets.googleplus_icon} width={50} alt='' />
-            </div>
+      <Image className='border-4 border-white' src={data.image} width={1280} height={720} alt='' />
+      <div className='blog-content' dangerouslySetInnerHTML={{__html:data.description}}></div>
+      
+
+      <div className='my-24'>
+        <p className='text-black font font-semibold my-4'>Share this article on social media</p>
+        <div className='flex'>
+          <Image src={assets.facebook_icon} width={50} alt='' />
+          <Image src={assets.twitter_icon} width={50} alt='' />
+          <Image src={assets.googleplus_icon} width={50} alt='' />
         </div>
+      </div>
     </div>
-    <Footer/>
-    </>:<></>
+    <Footer />
+  </> : <></>
   )
 }
 
