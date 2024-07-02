ed5b66dee3d5e282f6fa1a73e54f005af2babc1f
Lian Wiebe <lnwiebe05@gmail.com>
2024-07-02 10:53:01 +0200

    delete blog

--- Components/AdminComponents/BlogTableItem.jsx
+++ Components/AdminComponents/BlogTableItem.jsx
@@ -2,7 +2,8 @@ import { assets } from '@/Assets/assets'
 import Image from 'next/image'
 import React from 'react'
 
-const BlogTableItem = ({authorImg,title,author}) => {
+const BlogTableItem = ({authorImg,title,author,date,deleteBlog,id,mongoId}) => {
+    const BlogDate = new Date(date);
   return (
     <tr className='bg-white border-b'>
       <th scope='row' className='items-center gap-3 hidden sm:flex px-6 py-4 font-medium text-gray-900 whitespace-nowrap'>
@@ -13,9 +14,9 @@ const BlogTableItem = ({authorImg,title,author}) => {
         {title?title:"no title"}
       </td>
       <td className='px-6 py-4'>
-        {"11 Jan 2024"}
+        {BlogDate.toDateString()}
       </td>
-      <td className='px-6 py-4 cursor-pointer'>
+      <td onClick={()=>deleteBlog(mongoId)} className='px-6 py-4 cursor-pointer'>
         x
       </td>
     </tr>
--- app/api/blog/route.js
+++ app/api/blog/route.js
@@ -2,6 +2,7 @@ import { ConnectDB } from "@/lib/config/db";
 import BlogModel from "@/lib/models/BlogModel";
 const { NextResponse } = require("next/server");
 import {writeFile} from 'fs/promises'
+const fs = require('fs')
 
 const LoadDB = async () =>{
     await ConnectDB();
@@ -51,4 +52,14 @@ export async function POST(request){
     console.log("Blog Saved");
 
     return NextResponse.json({success:true,msg:"Blog Added"})
+}
+
+// Creating API Enpoint to delete Blog
+
+export async function DELETE(request){
+    const id = await request.nextUrl.searchParams.get('id');
+    const blog = await BlogModel.findById(id);
+    fs.unlink(`./public${blog.image}`,()=>{});
+    await BlogModel.findByIdAndDelete(id);
+    return NextResponse.json({msg:"Blog Deleted"});
 }
